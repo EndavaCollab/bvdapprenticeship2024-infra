47e26bf1f3140820298e44f170640129386f8cf8
mirunaenc <miruna.encuna@student.unitbv.ro>
2024-08-07 22:48:03 +0300

    Added security module
    - main.tf: Configure Network Security Group.
    - outputs.tf: Output nsg ID.
    - variables.tf: Define security variables.

---
+++ modules/security/variables.tf
@@ -0,0 +1,14 @@
+variable "nsg_name" {
+  description = "Name of the Network Security Group for the projects"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the Resource Group for the projects"
+  type        = string
+}
+
+variable "location" {
+  description = "Location of the resources for the projects"
+  type        = string
+}
