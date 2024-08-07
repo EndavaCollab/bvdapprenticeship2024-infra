929cbab4025854170f0471a34bb41befcb15d7f2
Vlad-Stefan Sas <me@svs.pm>
2024-08-07 14:47:22 +0300

    feat: (2) VM module now exports public IP

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -2,3 +2,8 @@ output "name" {
   value       = azurerm_linux_virtual_machine.server.name
   description = "Name of the created VM"
 }
+
+output "ip_address" {
+  value       = azurerm_linux_virtual_machine.server.public_ip_address
+  description = "Public IP address for the created VM"
+}
