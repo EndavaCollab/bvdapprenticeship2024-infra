a29b5ff0d186a61dbb2ebef041600ecc084d0f01
mirunaenc <miruna.encuna@student.unitbv.ro>
2024-08-08 14:44:57 +0300

    Revert "feat: (2) VM module now exports public IP"
    This reverts commit 929cbab4025854170f0471a34bb41befcb15d7f2.

--- modules/vm/outputs.tf
+++ modules/vm/outputs.tf
@@ -2,8 +2,3 @@ output "name" {
   value       = azurerm_linux_virtual_machine.server.name
   description = "Name of the created VM"
 }
-
-output "ip_address" {
-  value       = azurerm_linux_virtual_machine.server.public_ip_address
-  description = "Public IP address for the created VM"
-}
