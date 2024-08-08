77c0b4619de917f1352644943301940b757c096c
mirunaenc <miruna.encuna@student.unitbv.ro>
2024-08-08 14:44:47 +0300

    Revert "feat: (2) output JDBC string + added db"
    This reverts commit 71ff59b9c44e7c9680a02efcc5107ee14df5f41b.

--- modules/sql/variables.tf
+++ modules/sql/variables.tf
@@ -3,13 +3,8 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "server_name" {
-  description = "Name for the MySql server"
-  type        = string
-}
-
-variable "database_name" {
-  description = "Name for the MySql database to be created inside the server"
+variable "name" {
+  description = "Name for the database"
   type        = string
 }
 
