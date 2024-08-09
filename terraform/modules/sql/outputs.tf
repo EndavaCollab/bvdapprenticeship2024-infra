62582d722aae25c5e83d0a502bfedbecd6396024
Vlad-Stefan Sas <me@svs.pm>
2024-08-09 18:48:57 +0300

    fix: (2) string concat for JDBC conn strings

--- terraform/modules/sql/outputs.tf
+++ terraform/modules/sql/outputs.tf
@@ -1,9 +1,9 @@
 output "rcp_jdbc_string" {
-  value       = "jdbc:mysql://" + azurerm_mysql_flexible_server.server.fqdn + ":3306/" + azurerm_mysql_flexible_database.rcb_database.name
+  value       = join("", ["jdbc:mysql://", azurerm_mysql_flexible_server.server.fqdn, ":3306/", azurerm_mysql_flexible_database.rcb_database.name])
   description = "JDBC string to initialize the DB connection (Recipe book)"
 }
 
 output "exm_jdbc_string" {
-  value       = "jdbc:mysql://" + azurerm_mysql_flexible_server.server.fqdn + ":3306/" + azurerm_mysql_flexible_database.exm_database.name
+  value       = join("", ["jdbc:mysql://", azurerm_mysql_flexible_server.server.fqdn, ":3306/", azurerm_mysql_flexible_database.exm_database.name])
   description = "JDBC string to initialize the DB connection (Expense manager)"
 }
