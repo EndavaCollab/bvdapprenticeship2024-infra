ddf6fd024073a95dcf932baf034e68b5bc6cceb1
Miruna Ștefania Encună <miruna.encuna@student.unitbv.ro>
2024-08-12 14:24:41 +0300

    feat: (2) network & security modules (#3)
    * Added networking module
    
    - main.tf: Configure Virtual Network and Subnet.
    - outputs.tf: Output vnet and subnet IDs.
    - variables.tf: Define networking variables.
    
    * Added security module
    
    - main.tf: Configure Network Security Group.
    - outputs.tf: Output nsg ID.
    - variables.tf: Define security variables.
    
    * Added main configuration and global variables
    
    - main.tf: Integrate all modules and configure resource group and provider.
    - variables.tf: Define global variables for resource group and location.
    
    * Updated network and security configurations based on review feedback
    
    - Updated network security group (NSG) rules to include port 8080 for frontend and backend.
    - Modified variables to reflect correct port settings.
    - Implemented for_each in subnet configuration to allow creation of multiple subnets.
    - Adjusted output definitions to include subnet IDs.
    
    * Refactored network and security configurations based on review feedback
    
    - Moved main.tf to the root directory and renamed the resource group block to "rg".
    - Used existing Azure resource group with 'data' block instead of creating a new one.
    - Adjusted the networking module to use dynamic subnet configuration.
    - Combined redundant security rules for ports 80 and 8080 into a single rule.
    - Removed unnecessary output definitions and simplified module usage.
    
    * Refactored network and security configurations based on review feedback
    
    - Moved main.tf to the root directory and renamed the resource group block to "rg".
    - Used existing Azure resource group with 'data' block instead of creating a new one.
    - Adjusted the networking module to use dynamic subnet configuration.
    - Combined redundant security rules for ports 80 and 8080 into a single rule.
    - Removed unnecessary output definitions and simplified module usage.
    
    * Refactored networking and security configurations
    
    - Updated security rules to separate configurations for ports 80 and 8080.
    - Added support for multiple subnets in networking module.
    - Integrated subnet assignments directly into main.tf for better clarity and management.
    - Consolidated variables and ensured correct usage across modules.
    
    * chore: (2) add terraform generated folder to .gitignore
    
    * fix: (2) fix subnet list variable name
    
    * fix: (2) string concat for JDBC conn strings
    
    * fix: (2) mistaken .id on subnet id array
    
    * chore: (2) updated .gitignore to include .tfstate file
    
    * fix: (2) added blank feature block
    
    * chore: (2) removed defaults and useless variables + marked sensitive
    
    * feat: (2) added .tfvars file
    
    ---------
    
    Co-authored-by: Vlad-Stefan Sas <me@svs.pm>

---
+++ terraform/variables.tf
@@ -0,0 +1,91 @@
+variable "resource_group_name" {
+  description = "Name of the Resource Group for the projects"
+  type        = string
+}
+
+variable "location" {
+  description = "Location of the resources for the projects"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the Virtual Network for the projects"
+  type        = string
+}
+
+variable "address_space" {
+  description = "Address space for the Virtual Network"
+  type        = list(string)
+}
+
+variable "nsg_name" {
+  description = "Name of the Network Security Group for the projects"
+  type        = string
+  sensitive   = true
+}
+
+variable "admin_username" {
+  description = "Username for the admin user"
+  type        = string
+  sensitive   = true
+}
+
+variable "admin_ssh_public_key" {
+  description = "Public key for the admin user"
+  type        = string
+  sensitive   = true
+}
+
+variable "disk_size" {
+  description = "Disk size for the server (in GBs)"
+  type        = number
+}
+
+variable "rcb_vm_name" {
+  description = "Virtual Machine Name for Recipe Book project"
+  type        = string
+}
+
+variable "exm_vm_name" {
+  description = "Virtual Machine Name for Expense Manager project"
+  type        = string
+}
+
+variable "sql_server_name" {
+  description = "Name for the MySql server"
+  type        = string
+}
+
+variable "rcb_database_name" {
+  description = "Name for the MySql database to be created inside the server. (Recipe book)"
+  type        = string
+}
+
+variable "exm_database_name" {
+  description = "Name for the MySql database to be created inside the server (Expense manager)"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Password for the admin user (Database)"
+  type        = string
+  sensitive   = true
+}
+
+variable "backend_exm_port" {
+  description = "Port for Expense Manager backend application"
+  type        = number
+}
+
+variable "backend_rcb_port" {
+  description = "Port for Recipe Book backend application"
+  type        = number
+}
+
+variable "subnets" {
+  description = "Required subnets to deploy the VMs"
+  type = list(object({
+    name   = string,
+    prefix = string
+  }))
+}
