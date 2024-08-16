07c2c25de61b78b568989266f3bb0a69595a559e
Vlad-Stefan Sas <me@svs.pm>
2024-08-16 15:14:15 +0300

    fix: (4) added default sku name to mysql flexible server

--- terraform/modules/sql/main.tf
+++ terraform/modules/sql/main.tf
@@ -1,9 +1,9 @@
 resource "azurerm_mysql_flexible_server" "server" {
-  name                = var.server_name
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  administrator_login = var.admin_username
-  # TODO: password as secret
+  name                   = var.server_name
+  resource_group_name    = var.resource_group_name
+  location               = var.location
+  administrator_login    = var.admin_username
+  sku_name               = "B_Standard_B1s"
   administrator_password = var.admin_password
 }
 
