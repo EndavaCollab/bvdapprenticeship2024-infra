a0c8a94acf47f0b440356df9e5f99ec2637a1118
Vlad-Stefan Sas <me@svs.pm>
2024-08-21 14:55:37 +0300

    fix: (2) fix subnets path

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
   locals {
-    subnets = concat(var.virtual_machines[*].subnets)
+    subnets = concat(var.virtual_machines[*].networking.subnets)
   }
 
 resource "azurerm_subnet" "subnet" {
