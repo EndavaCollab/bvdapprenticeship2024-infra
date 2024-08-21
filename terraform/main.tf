04580e7638c0594fa758af231ace37a62ce72434
Vlad-Stefan Sas <me@svs.pm>
2024-08-21 14:39:19 +0300

    fix: (2) converted vms

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,26 +18,40 @@ module "security" {
   security_groups = [for vm in var.virtual_machines : vm.networking.network_interface.security_group]
 }
 
-module "rcb_vm" {
-  source               = "./modules/vm"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  server_name          = var.rcb_vm_name
-  admin_username       = var.admin_username
-  admin_ssh_public_key = var.admin_ssh_public_key
-  disk_size            = var.disk_size
-  network_interface_id = module.networking.network_interface_ids[0]
-}
+# module "rcb_vm" {
+#   source               = "./modules/vm"
+#   resource_group_name  = var.resource_group_name
+#   location             = var.location
+#   server_name          = var.rcb_vm_name
+#   admin_username       = var.admin_username
+#   admin_ssh_public_key = var.admin_ssh_public_key
+#   disk_size            = var.disk_size
+#   network_interface_id = module.networking.network_interface_ids[0]
+# }
 
-module "exm_vm" {
-  source               = "./modules/vm"
-  resource_group_name  = var.resource_group_name
-  location             = var.location
-  server_name          = var.exm_vm_name
-  admin_username       = var.admin_username
+# module "exm_vm" {
+#   source               = "./modules/vm"
+#   resource_group_name  = var.resource_group_name
+#   location             = var.location
+#   server_name          = var.exm_vm_name
+#   admin_username       = var.admin_username
+#   admin_ssh_public_key = var.admin_ssh_public_key
+#   disk_size            = var.disk_size
+#   network_interface_id = module.networking.network_interface_ids[1]
+# }
+
+module "vm" {
+  count = length(var.virtual_machines)
+  for_each = [for vm in var.virtual_machines : vm]
+
+  source = "./modules/vm"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  server_name = var.virtual_machines[count.index].name
+  admin_username = var.admin_username
   admin_ssh_public_key = var.admin_ssh_public_key
-  disk_size            = var.disk_size
-  network_interface_id = module.networking.network_interface_ids[1]
+  disk_size = var.virtual_machines[count.index].disk_size
+  network_interface_id = module.networking.network_interface_ids[count.index]
 }
 
 module "sql" {
