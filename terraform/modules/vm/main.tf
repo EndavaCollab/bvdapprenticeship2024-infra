0f25e61156a1405f75a0ce56f012cd7a8b2ce525
Vlad-Stefan Sas <me@svs.pm>
2024-08-23 18:00:36 +0300

    fix: (2) change public ip name

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "${var.vm_name}-ip"
+  name                = "${var.vm_name}-public-ip"
   resource_group_name = var.resource_group_name
   location            = var.location
   allocation_method   = var.public_ip_allocation_method
