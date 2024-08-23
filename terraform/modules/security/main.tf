94a9176209a786b2d56bcdb0ddf6136edf8cde5a
Vlad-Stefan Sas <me@svs.pm>
2024-08-23 17:40:15 +0300

    fix: (2) changed to count

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_subnet_network_security_group_association" "nsg-subnet-association" {
-  for_each                  = var.subnet_ids
-  subnet_id                 = each.value
+  count                     = length(var.subnet_ids)
+  subnet_id                 = var.subnet_ids[count.index]
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
